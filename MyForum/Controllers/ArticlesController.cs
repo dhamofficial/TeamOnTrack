7bff846e4396c661b454b03a218a04523c967664
dhamofficial@hotmail.com <dhamofficial@gmail.com>
2014-09-06 08:50:00 +0530

    new actions added

--- MyForum/Controllers/ArticlesController.cs
+++ MyForum/Controllers/ArticlesController.cs
@@ -1,4 +1,5 @@
-﻿using MyForum.Models;
+﻿using MyForum.Entities;
+using MyForum.Models;
 using MyForum.Viewmodel;
 using System;
 using System.Collections.Generic;
@@ -10,31 +11,66 @@ namespace MyForum.Controllers
 {
     public class ArticlesController : Controller
     {
+        private SearchFilter filter { get; set; }
+
+        public ArticlesController()
+        {
+            
+            filter = new SearchFilter();
+        }
+
         // GET: Articles
         public ActionResult Index()
         {
-            ArticlesContainer viewModel = new ArticlesContainer();
-            ArticlesRepository obj = new ArticlesRepository();
-            viewModel.FeaturedArticles = obj.GetFeaturedList();
+            var viewModel = GetArticleContainer(filter);
 
             return View(viewModel);
         }
 
         public ActionResult Favorites()
         {
-            ArticlesContainer viewModel = new ArticlesContainer();
-            ArticlesRepository obj = new ArticlesRepository();
-            viewModel.FeaturedArticles = obj.GetFeaturedList();
+            var viewModel = GetArticleContainer(filter);
 
             return View(viewModel);
         }
 
+        public ActionResult Category(int? id)
+        {
+            filter.CategoryID = Convert.ToInt32(id);
+            var viewModel = GetArticleContainer(filter);
+
+            return View("Search",viewModel);
+        }
+
+        public ActionResult Team(int? id)
+        {
+            filter.TeamID = Convert.ToInt32(id);
+            var viewModel = GetArticleContainer(filter);
+
+            return View("Search", viewModel);
+        }
+
+        public ActionResult Search()
+        {
+            return View();
+        }
+
         public ActionResult NewPost()
         {
-
-
-
             return View();
+        }
+
+        ArticlesContainer GetArticleContainer(SearchFilter filter)
+        {
+            ArticlesContainer viewModel = new ArticlesContainer();
+            ArticlesRepository obj = new ArticlesRepository();
+            TeamRepository teamObj = new TeamRepository();
+
+            viewModel.FeaturedArticles = obj.SearchArticles(filter);
+            viewModel.Categories = obj.GetCategories();
+            viewModel.Teams = teamObj.GetTeams();
+
+            return viewModel;
         }
     }
 }
