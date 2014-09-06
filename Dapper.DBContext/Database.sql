afb780ecbba1d6cc53c42a94bd64181187532174
dhamofficial@hotmail.com <dhamofficial@gmail.com>
2014-09-06 08:51:56 +0530

    Functionality Implementation and Other Optimizations

--- Dapper.DBContext/Database.sql
+++ Dapper.DBContext/Database.sql
@@ -35,3 +35,28 @@ GO
 SET ANSI_PADDING OFF
 GO
 
+
+
+USE [teamontrack]
+GO
+
+/****** Object:  Table [dbo].[Categories]    Script Date: 9/4/2014 9:09:16 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Categories](
+	[UID] [int] IDENTITY(1,1) NOT NULL,
+	[CategoryName] [nvarchar](1000) NULL,
+	[Description] [nvarchar](2000) NULL,
+	[Active] [bit] NULL,
+ CONSTRAINT [PK_Categories] PRIMARY KEY CLUSTERED 
+(
+	[UID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
