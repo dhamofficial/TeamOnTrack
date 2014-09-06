ee6b14d6ccf28d96163d8f0ab0feae1543b26f00
dhamofficial@hotmail.com <dhamofficial@gmail.com>
2014-09-06 08:50:57 +0530

    Functinality Implementation and other optimizations

---
+++ MyForum/WebAPI/AsyncController.cs
@@ -0,0 +1,51 @@
+﻿using MyForum.Entities;
+using MyForum.Models;
+using MyForum.Viewmodel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MyForum.WebAPI
+{
+    public class AsyncController : ApiController
+    {
+
+        public IEnumerable<Article> Category(int? id)
+        {
+            ArticlesContainer viewModel = new ArticlesContainer();
+            ArticlesRepository obj = new ArticlesRepository();
+            return obj.GetFeaturedList();
+        }
+
+
+        // GET: api/Async
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Async/5
+        public string Get(int? id)
+        {
+            return "value";
+        }
+
+        // POST: api/Async
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Async/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Async/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
