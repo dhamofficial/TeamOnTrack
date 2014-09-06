ab965b3d46188a9396d3c3650d4f16c7fb3f57bf
dhamofficial@hotmail.com <dhamofficial@gmail.com>
2014-09-06 08:49:12 +0530

    teams repository

---
+++ MyForum/Models/TeamRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Dapper;
+using MyForum.Entities;
+using System.Text;
+
+namespace MyForum.Models
+{
+    public class TeamRepository
+    {
+        public IEnumerable<Team> GetTeams()
+        {
+            IEnumerable<Team> list;
+
+            using (var dbContext = new DBContext())
+            {
+                list = dbContext.Connection.Query<Team>(@" select UID,[TeamName] from Teams where active = 1", new { });
+            }
+
+            return list;
+        }
+         
+    }
+}
