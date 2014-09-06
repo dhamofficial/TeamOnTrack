076bdad4a01faa2d8bc894fd1e243b0a24c7db2c
dhamofficial@hotmail.com <dhamofficial@gmail.com>
2014-09-06 08:48:59 +0530

    new actions added

--- MyForum/Models/ArticlesRepository.cs
+++ MyForum/Models/ArticlesRepository.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using Dapper;
 using MyForum.Entities;
+using System.Text;
 
 namespace MyForum.Models
 {
@@ -16,6 +17,35 @@ namespace MyForum.Models
             using (var dbContext = new DBContext())
             {
                 list = dbContext.Connection.Query<Article>(@" select UID,[Title] from Articles  ", new { });
+            }
+
+            return list;
+        }
+
+        public IEnumerable<Article> SearchArticles(SearchFilter filter)
+        {
+            IEnumerable<Article> list;
+
+            StringBuilder sqlWhere = new StringBuilder();
+
+            if (filter.CategoryID>0)
+                sqlWhere.AppendFormat(" and CategoryId=@CategoryId");
+
+            using (var dbContext = new DBContext())
+            {
+                list = dbContext.Connection.Query<Article>(@" select UID,[Title] from Articles where 1=1 " + sqlWhere, new { CategoryID=filter.CategoryID });
+            }
+
+            return list;
+        }
+
+        public IEnumerable<Category> GetCategories()
+        {
+            IEnumerable<Category> list;
+
+            using (var dbContext = new DBContext())
+            {
+                list = dbContext.Connection.Query<Category>(@" select UID,[CategoryName] from Categories where active = 1  ", new { });
             }
 
             return list;
