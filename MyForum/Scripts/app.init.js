d89485ee91702f1d97724fe3161e880ac04afb54
dhamofficial@hotmail.com <dhamofficial@gmail.com>
2014-09-06 08:20:58 +0530

    active class added on selection of main menu items

--- MyForum/Scripts/app.init.js
+++ MyForum/Scripts/app.init.js
@@ -10,25 +10,38 @@ app.config(['$routeProvider',function ($routeProvider) {
     .when('/', { templateUrl: appname+'/articles', controller: 'homeCtrl' })
     .when('/favorites', { templateUrl: appname + '/articles/favorites', controller: 'FavoritesCtrl' })
     .when('/newpost', { templateUrl: appname + '/articles/newpost', controller: 'FavoritesCtrl' })
+    .when('/category/:categoryId', { templateUrl: function (param) { return appname + '/articles/Category/' + param.categoryId }, controller: 'SearchCtrl' })
     .when('/login', { templateUrl: appname + '/login', controller: 'LoginCtrl' })
     .when('/logout', { templateUrl: 'admin/login.html', controller: 'LogoutCtrl' })
     .otherwise({ redirectTo: '/' });
 }]);
 
-app.controller("homeCtrl", ['$scope',function ($scope) {
-    $scope.model = {
-        message: "This is my app!!!"
-    }
+app.controller("homeCtrl", ['$scope', '$routeParams', function ($scope, $routeParams) {
+    
 }]);
 
-app.controller("FavoritesCtrl", ['$scope', function ($scope) {
-    $scope.model = {
-        message: "This is my FavoritesCtrl app!!!"
-    }
+app.controller("FavoritesCtrl", ['$scope', '$routeParams', function ($scope, $routeParams) {
+    $scope.categoryId = $routeParams.categoryId;
 }]);
   
+app.controller("SearchCtrl", ['$scope', '$routeParams', function ($scope, $routeParams) {
+    $scope.categoryId = $routeParams.categoryId;
+}]);
+
 app.controller("LoginCtrl", ['$scope', function ($scope) {
     $scope.model = {
         message: "This is login page"
     }
 }]);
+$(document).ready(function () {
+    $('.nav li a').click(function (e) {
+
+        $('.nav li').removeClass('active');
+
+        var $parent = $(this).parent();
+        if (!$parent.hasClass('active')) {
+            $parent.addClass('active');
+        }
+        e.preventDefault();
+    });
+});
